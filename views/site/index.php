84fe9482c34cdda9ea445727f0191ef80bbd703e
fernandrez <fernandrez@gmail.com>
2016-03-25 18:24:01 -0300

    Actualizar registros cuando se actualice una categoria, almacen o cadena

--- views/site/index.php
+++ views/site/index.php
@@ -7,6 +7,7 @@ $this->title = 'IPC BsAs';
 <div class="site-index">
     <ul class="nav nav-pills">
         <li><?= Html::a("Registros",'registro'); ?></li>
+        <li><?= Html::a("Categorías",'registro/categoria'); ?></li>
         <li><?= Html::a("Cadenas",'registro/cadena'); ?></li>
         <li><?= Html::a("Almacenes",'registro/almacen'); ?></li>
     </ul>
